bdbb7a7d1cc4454f63be8b884ee6c9b6420a7300
Christian Tumandao <christianptumandao@gmail.com>
2023-04-01 21:53:23 -0400

    Added board and board button functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
 import { useMediaQuery } from 'react-responsive';
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
 
 import RightDisplay from "./components/rightDisplay";
 import { Chessboard } from 'react-chessboard';
-import { Chess } from 'chess.js';
+import { Chess } from "chess.js";
+import axios from 'axios';
 
 function App() {
 
+  const [game, setGame] = useState(new Chess());
+
   const [colorOrientation, setOrientation] = useState('white');
+  const [undoBool, setUndo] = useState(false);
+
+  const [rightDisplayHeader, setRightDisplayHeader] = useState('Chess Opening Flashcards');
+  const [rightDisplaySubheader, setRightDisplaySubheader] = useState([]);
+  const [rightDisplayTabs, setRightDisplayTabs] = useState([]); // MUST BE array to map properly
+  const [displayTabs, setDisplayTabs] = useState(false); // display tabs ? true : false
+  const axios_base_url = "https://raw.githubusercontent.com/hayatbiralem/eco.json/master/eco.json";
+
+  const [history, setHistory] = useState("");
+  const [moveCount, setMoveCount] = useState(0);
   
+  useEffect(()=>{
+    console.log(history);
+  }, [history])
+
+  useEffect(() => {
+    let currFen = game.fen(); 
+    currFen = currFen.substring(0, currFen.length -6);
+    let url_fen = currFen.replaceAll(' ', "%20");
+    console.log("FEN:", currFen);
+
+    let axios_url = axios_base_url+"?moves="+url_fen;
+    console.log("url: ", axios_url);
+    
+  }, [game.fen()])
+
+  const handleMove = (fromSquare, toSquare) => {
+    try {
+      var newGame = new Chess(game.fen());
+      var move = newGame.move({
+          from: fromSquare,
+          to: toSquare,
+          promotion: 'q' 
+      });
+      if (game.turn()==='w') {
+        setHistory(history + " "+String(moveCount+1)+'. '+toSquare);
+        setMoveCount(moveCount+1);
+      }
+      else if (game.turn()==='b') {
+        setHistory(history+" "+toSquare);
+      }
+      
+      setGame(newGame);
+    }
+    catch {
+      console.log("move failed");
+    }
+    
+  
+
+  }
+
   const swapOreintation = () => {
     if (colorOrientation==='white') setOrientation('black');
     else setOrientation('white');
   }
+  const handleUndo =() => {
+      setUndo(true);
+  }
+  const handleReset = () => {
+    setGame(new Chess());
+    setHistory('');
+    setMoveCount(0);
+  }
+
 
   return (
     <div className="App">
@@ -28,35 +91,52 @@ function App() {
             <div id="left-display">
 
               <div id="left-display-header">
-                  left display header
+                 { } 
               </div>
 
               <div id="board" >
            
-                <Chessboard 
+               <Chessboard 
                   boardWidth={500} 
-                  boardOrientation = {colorOrientation}
-                  getPositionObject = {  (currentPosition) => { console.log(currentPosition)}}
 
+                  position = { game.fen() }
+                  onPieceDrop = { handleMove }
+
+                  boardOrientation = { colorOrientation }
+                  
+                  showNotation = { true }
+     
+                  
 
                  /> 
+                
               </div> 
 
               <div id="board-buttons">
    
-                <button id="undo">Undo</button>
+                <button id="undo"
+                  onClick = { handleUndo }
+                  >Undo</button>
+
                 <button id ="flip"
-                  onClick = {swapOreintation}
+                  onClick = { swapOreintation }
                   >Flip
                   </button>
-                <button id="reset">Reset</button>
+
+                <button id="reset"
+                  onClick = { handleReset }
+                >Reset</button>
               </div>
 
             </div>
 
 
             <div id="right-display">
-              <RightDisplay />
+              <RightDisplay
+                rightDisplayHeader = { rightDisplayHeader }
+                rightDisplayTabs = { rightDisplayTabs }
+                displayTabs = { displayTabs }
+              />
             </div>
 
           </div>
--- src/components/rightDisplay.jsx
+++ src/components/rightDisplay.jsx
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
 
 class rightDisplay extends Component {
-    state = {  } 
     render() { 
         return (
-            <h1>Right Display</h1>
+            <React.Fragment>
+
+                {/**right display header */}
+                <div id ="right-display-header">
+                    {
+                        this.props.rightDisplayHeader
+                    }
+                </div>
+
+                {/**right display subheader */}
+                <div id ="right-display-subheader">
+                    <button id="explore-button">Explore Openings</button>
+                    <button id="search-button">Search Openings</button>
+                    <button id="login-button">Login/Register</button>
+                </div>
+
+                {/** right display tabs */}
+                <div className = { this.getClasses("right-display-tabs") }>
+                    { 
+                        this.props.rightDisplayBody
+                    }
+                </div>
+
+                {/** right display message body */}
+                <div className = { this.getClasses('right-display-message-body')}>
+                    <div className="app-description">
+                        <p>
+                            Easily explore, study, and test yourself on chess openings
+                        </p>
+                        <p id="get-started">
+                            Start by playing a move or clicking the "Search Openings" tab.
+                        </p>
+                    </div>
+                    <div className="tech-stack">
+                        <p>
+                            This web application uses the lichess api for chess openings.
+                        </p>
+                        <p>
+                            Built on React, Axios, Spring Boot MVC, Spring data jpa, AWS RDS (mySQL), AWS S3, and AWS EBS
+                        </p>
+                        <p>Christian Tumandao</p>
+                    </div>
+                    
+                </div>
+
+            </React.Fragment>
         );
+    }
+
+    getClasses = (divType) => {
+        
+        if (divType === "right-display-tabs") {
+            if (this.props.displayTabs === true) return divType; 
+            else return 'display-none';
+        }
+        else if (divType ==="right-display-message-body") {
+            if (this.props.displayTabs === false) return divType; 
+            else return 'display-none';
+        }
     }
 }
  
